a52713b86585865a860fca2d2db08c1d752e148b
mfort-cen76304 <111354468+mfort-cen76304@users.noreply.github.com>
2026-06-02 12:26:22 +0000

    feat(quiz): add nickname page scenario

--- specs/src/pages/quiz-nickname-page.ts
+++ specs/src/pages/quiz-nickname-page.ts
@@ -1,5 +1,3 @@
-import { type Page } from '@playwright/test'
-
 export class QuizNicknamePage {
-    constructor(private page: Page) {}
+    constructor() {}
 }
--- specs/src/steps/world/world.ts
+++ specs/src/steps/world/world.ts
@@ -35,7 +35,7 @@ export class QuizmasterWorld {
         this.quizScorePage = new QuizScorePage(this.page)
         this.workspacePage = new WorkspacePage(this.page)
         this.quizCreatePage = new QuizCreatePage(this.page)
-        this.quizNicknamePage = new QuizNicknamePage(this.page)
+        this.quizNicknamePage = new QuizNicknamePage()
         this.homePage = new HomePage(this.page)
     }
 
